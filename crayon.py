db734702818119d25905835ac044f7749ab87433
Derrick Ishman <derrick.ishman@gmail.com>
2024-06-04 16:19:09 -0500

    changed .gitignore

---
+++ crayon.py
@@ -0,0 +1,14 @@
+import colorama
+
+# Auto reset color back to white after color call
+colorama.init(autoreset=True)
+
+# Colors we use
+c_BLUE = f"{colorama.Style.BRIGHT}{colorama.Fore.BLUE}"
+c_GREEN = f"{colorama.Style.BRIGHT}{colorama.Fore.GREEN}"
+c_MAGENTA = f"{colorama.Style.BRIGHT}{colorama.Fore.MAGENTA}"
+c_CYAN = f"{colorama.Style.BRIGHT}{colorama.Fore.CYAN}"
+c_RED = f"{colorama.Style.BRIGHT}{colorama.Fore.RED}"
+c_YELLOW = f"{colorama.Style.BRIGHT}{colorama.Fore.YELLOW}"
+c_WHITE = f"{colorama.Style.BRIGHT}{colorama.Fore.WHITE}"
+
