0c53ae12750908eb2a294cd7f2d32887ae37e838
Derrick Ishman <derrick.ishman@gmail.com>
2024-09-27 09:07:53 -0500

    Changed f-string for affected() function

--- low_rx_opt_pwr.py
+++ low_rx_opt_pwr.py
@@ -56,17 +56,22 @@ def cx_detail(e9, tbl, cnct):
         rlen = int(r.get('range-length')) / 1000
         return f'\nONT: {port}\nUS-Light: \t{up_rx}\nDS-Light: \t{dn_rx}\nUS-BER: \t{up_ber}\nDS-BER: \t{dn_ber}\nRange: {rlen}km\nSN: {sn}\nPON-Port: {lpon}\nUptime: {h}hours {m}minutes {s}seconds\nLast-Restart: {lr}\n'
 
-    def affected(e9, instid):
+    def affected(e9, instid, port):
         get_affected = get(f'https://10.20.7.10:18443/rest/v1/fault/export/csv/subscriber/device-name/{e9}/instance-id/{instid}',
                            auth=('admin', 'Thesearethetimes!'), verify=False)
         r = get_affected.text.split('\r\n')
         for i in r[1:-1]:
+            get_phone = get(f'https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{e9}/port/{port}%2Fx1',
+                            auth=('admin', 'Thesearethetimes!'),
+                            verify=False)
+            r1 = get_phone.json()
+            phone = r1.get('locations')[0].get('contacts')[0].get('phone')
             sp = i.split(',')
             acct = sp[0]
             name = sp[1]
             loc = ' '.join(sp[2:5])
             em = sp[-1]
-        return f'{acct}\n{name}\n{loc}\n{em}'
+        return f'{acct}\n{name}\n{phone}\n{em}\n{loc}'
 
     def email(e9, instid, port):
         import smtplib
@@ -86,18 +91,11 @@ def cx_detail(e9, tbl, cnct):
         from subprocess import run
         path.append(
             '/home/derrick/Documents/CVEC_Stuff/low-rx-pwr/')
-        with cnct as con:
-            con.send_command_timing(
-                f'manual shelve instance-id {instid}')
-        p1 = run('ls *.txt',
-                 text=True,
-                 shell=True,
-                 capture_output=True
-                 )
-        for _ in p1.stdout.split():
-            run(f'mv {e9}_{instid}.txt {path[-1]}/{e9}/{e9}_{instid}.txt',
-                shell=True
-                )
+        cnct.send_command_timing(
+            f'manual shelve instance-id {instid}')
+        run(f'mv {e9}_{instid}.txt {path[-1]}/{e9}/{e9}_{instid}.txt',
+            shell=True
+            )
 
     def process_alarm(tbl):
         alarms = tbl.split('\n')
@@ -108,12 +106,13 @@ def cx_detail(e9, tbl, cnct):
             instid, gport = data[7], data[13].split("'")
             port = gport[1]
             ont_info = ont_detail(e9, port)
-            cx_info = affected(e9, instid)
+            cx_info = affected(e9, instid, port)
             all_info = f'{cx_info}\n{ont_info}'
             with open(f'{e9}_{instid}.txt', 'w') as f:
                 f.write(all_info)
             email(e9, instid, port)
             clean_up(e9, instid, cnct)
+        cnct.disconnect()
 
     process_alarm(tbl)
 
