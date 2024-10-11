ae22f965fa91dc6c87815ad5fd28e97bcd3a150b
derrick <twohackerauth@proton.me>
2024-10-11 00:27:30 -0500

    Changed entry point function name

--- e9_all_on.py
+++ e9_all_on.py
@@ -7,27 +7,28 @@ from sys import argv
 
 
 # Call with starting shelf and ending shelf
-def shelf():
-    device = {'device_type': 'cisco_ios',
-              'host':   '192.168.1.1',
-              'username':   'sysadmin',
-              'password':   'sysadmin',
-              'fast_cli':   False,
-              }
+def turn_on_ports():
+    device = {
+        "device_type": "cisco_ios",
+        "host": "192.168.1.1",
+        "username": "sysadmin",
+        "password": "sysadmin",
+        "fast_cli": False,
+    }
     shelves = range(int(argv[1]), int(argv[2]) + 1)
     slot = range(1, 3)
     port = range(1, 17)
     with ConnectHandler(**device) as cnct:
-        cnct.send_command_timing('configure')
+        cnct.send_command_timing("configure")
         for shelf in shelves:
             for sl in slot:
                 for p in port:
                     cnct.send_command_timing(
-                        f'interface pon {shelf}/{sl}/xp{p}')
-                    cnct.send_command_timing('no shutdown')
-                    cnct.send_command_timing('top')
-                    print(f'{shelf}/{sl}/xp{p} is on')
+                        f"interface pon {shelf}/{sl}/xp{p}")
+                    cnct.send_command_timing("no shutdown")
+                    cnct.send_command_timing("top")
+                    print(f"{shelf}/{sl}/xp{p} is on")
 
 
-if __name__ == '__main__':
-    shelf()
+if __name__ == "__main__":
+    turn_on_ports()
