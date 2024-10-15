6248d69b5d9aa285c335bbb71bad4514fdf78fce
Derrick Ishman <derrick.ishman@gmail.com>
2024-10-15 15:05:41 -0500

    Adding a missing check for get_act_cx.py

--- get_act_cx.py
+++ get_act_cx.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 
-from netmiko import ConnectHandler
 from requests import get
-from sys import argv, path
+from sys import argv
+from calix import ssp, netcon
 
 
-# Call script with IP address, hostname
-
-
-def get_info():
-    path.append('/home/derrick/Derrick-shell-scripts/python/modules/')
-    from crayon import c_BLUE, c_WHITE
-    shelf = (input(f'{c_BLUE}Shelf: {c_WHITE}'))
-    slot = (input(f'{c_BLUE}Slot: {c_WHITE}'))
-    port = (input(f'{c_BLUE}Port: {c_WHITE}'))
-    return shelf, slot, port
+# NOTE: Call script with IP address, hostname
 
 
 def get_act(func):
@@ -38,11 +29,13 @@ def get_act(func):
         else:
             print('Went here')
             for lst in onts:
+                if lst is None:
+                    continue
                 for id in lst:
-                    get_phone = get(f'https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{argv[2]}/port/{id}%2Fx1',
-                                    auth=('admin', 'Thesearethetimes!'),
-                                    verify=False)
-                    r = get_phone.json()
+                    get_cx = get(f'https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{argv[2]}/port/{id}%2Fx1',
+                                 auth=('admin', 'Thesearethetimes!'),
+                                 verify=False)
+                    r = get_cx.json()
                     if r.get('locations') is None:
                         continue
                     loc, city, state, z = r.get('locations')[
@@ -55,49 +48,35 @@ def get_act(func):
     return wrapper
 
 
-def get_miss(func):
-    def wrapper1(*args):
-        print('This is the get_miss function')
-        onts = func(*args)
-        missing = get(f'https://10.20.7.10:18443/rest/v1/config/device/{argv[2]}/ont/missingONTs?offset=0&limit=20',
-                      auth=('admin', 'Thesearethetimes!'),
-                      verify=False)
-        r = missing.json()
-        for i in r:
-            ont_id = i.get('ont-id')
-            print(ont_id)
-        return onts
-    return wrapper1
-
-
 @get_act
-def netcon(shelf, slot, port):
-    device = {'device_type': 'cisco_ios',
-              'host':   f'{argv[1]}',
-              'username':   'sysadmin',
-              'password':   'Thesearethetimes!',
-              'fast_cli':   False,
-              }
+def get_miss(main_f):
+    def missing():
+        func = main_f()
+
+
+# @get_act
+def main():  # NOTE: Returns list[lists], or a single list
+    shelf, slot, port = ssp()
+    cnct = netcon()
     ports = range(1, 17)
     slots = range(1, 3)
-    cnct = ConnectHandler(**device)
+    step_2 = slice(1, None, 2)
     if not slot == "" and port == "":
         onts = (cnct.send_command_timing(
             f'show interface pon {shelf}/{slot}/xp{p} ranged-onts \
-            statistics | inc ont-id').split()[1::2]
+            statistics | inc ont-id').split()[step_2]
             for p in ports)
     elif slot == "":
         onts = (cnct.send_command_timing(
             f'show interface pon {shelf}/{sl}/xp{p} ranged-onts \
-            statistics | inc ont-id').split()[1::2]
+            statistics | inc ont-id').split()[step_2]
             for sl in slots
             for p in ports)
     else:
         onts = cnct.send_command_timing(
             f'show interface pon {shelf}/{slot}/xp{port} ranged-onts \
-            statistics | inc ont-id').split()[1::2]
+            statistics | inc ont-id').split()[step_2]
     return onts
 
 
-shelf, slot, port = get_info()
-netcon(shelf, slot, port)
+main()
