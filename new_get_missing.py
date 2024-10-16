50bd8248373ff1b332284938f0a11f2c0902259e
derrick <twohackerauth@proton.me>
2024-10-16 13:28:08 -0500

    Writing new missing function

--- new_get_missing.py
+++ new_get_missing.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 from calix.cx_detail import cx
+from calix.ont_detail import ont
 
 
-r = cx("Ingram-E9-1", "10517")
-print(r)
+r = cx("Liberty-E9-1", "82")
+a = ont("Liberty-E9-1", "82")
+print(f"{r}\n")
+print(a.get("oper-status"))
