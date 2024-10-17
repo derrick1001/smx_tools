c309f10cb79feff0c1364879a5cb1dbfd0dee717
derrick <twohackerauth@proton.me>
2024-10-16 22:04:27 -0500

    Saved changes for the day, working on customer outage list generation

--- new_get_missing.py
+++ new_get_missing.py
@@ -8,10 +8,17 @@ from calix.connection import calix_e9
 from calix.affected_decorator import affected_decorator
 
 
+b = cx("Ingram-E9-1", "3987")
+print(b)
+
+
 @affected_decorator
 def f(a, b, c):
     pass
 
 
-a = f("Seminole-E9-1", ["10.33491", "10.33765"], "11518")
-print(a)
+f(
+    "Ingram-E9-1",
+    ["13.107546", "13.105931", "13.67267", "13.61459"],
+    ["6979", "11833", "3987", "7692"],
+)
