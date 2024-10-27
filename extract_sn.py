9201d38f1c7a07834fb4e3adc9366dee3a2ab552
derrick <twohackerauth@proton.me>
2024-10-26 20:52:13 -0500

    Cleaned this repo to only include projects we are testing

--- extract_sn.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import re
-
-with open('calix-sn.txt') as f:
-    for line in f:
-        m = re.search('sn=\\d{12}', line)
-        if m:
-            print(m.group().lstrip('sn='))
