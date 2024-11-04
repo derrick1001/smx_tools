82cff20e47238c4a9dfd4d230c63db5921946d60
Derrick Ishman <twohackerauth@proton.me>
2024-11-03 21:06:58 -0600

    debug: Testing loss-of-pon parsing

--- get_missing.py
+++ get_missing.py
@@ -13,17 +13,17 @@ from crayon import c_BLUE, c_WHITE, c_YELLOW
 
 
 def proc_alarms(func):
-    @affected_decorator
+    # @affected_decorator
     def inner(**kwargs):
         alrm_tbl = func()
         if "loss-of-pon" in alrm_tbl:
-            match_pon = [
+            match_pon = (
                 re.search("[2-5]/[1-2]/xp[0-9]{1,2}", alrm)
                 for alrm in alrm_tbl.split("\n")
-            ]
-            pon_port = [
+            )
+            pon_port = (
                 m.group().lstrip("'").rstrip("'") for m in match_pon if m is not None
-            ]
+            )
             cnct = calix_e9()
             sub_on_port = [
                 cnct.send_command_timing(
@@ -31,10 +31,8 @@ def proc_alarms(func):
                 )
                 for port in pon_port
             ]
-            match_ont = [re.search("[0-9]{1,5}", id) for id in sub_on_port]
-            ont_id = [
-                m.group().lstrip("'").rstrip("'") for m in match_ont if m is not None
-            ]
+            print(sub_on_port)
+            ont_id = [re.search("[0-9]{1,5}", id) for id in sub_on_port]
             return ont_id
         else:
             match_ont = [
