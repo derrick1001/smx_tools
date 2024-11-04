20e8e251f1377969f8af4eb6167f085ff7510bd8
Derrick Ishman <twohackerauth@proton.me>
2024-11-03 20:17:45 -0600

    feature: Alarm name now called and acted upon, also counts the subs printed

--- get_missing.py
+++ get_missing.py
@@ -16,9 +16,34 @@ def proc_alarms(func):
     @affected_decorator
     def inner(**kwargs):
         alrm_tbl = func()
-        match = [re.search("'[0-9]{1,5}'", alrm) for alrm in alrm_tbl.split("\n")]
-        ont_id = [m.group().lstrip("'").rstrip("'") for m in match if m is not None]
-        return ont_id
+        if "loss-of-pon" in alrm_tbl:
+            match_pon = [
+                re.search("[2-5]/[1-2]/xp[0-9]{1,2}", alrm)
+                for alrm in alrm_tbl.split("\n")
+            ]
+            pon_port = [
+                m.group().lstrip("'").rstrip("'") for m in match_pon if m is not None
+            ]
+            cnct = calix_e9()
+            sub_on_port = [
+                cnct.send_command_timing(
+                    f"show interface pon {port} subscriber-info | display curly-braces | include ont"
+                )
+                for port in pon_port
+            ]
+            match_ont = [re.search("[0-9]{1,5}", id) for id in sub_on_port]
+            ont_id = [
+                m.group().lstrip("'").rstrip("'") for m in match_ont if m is not None
+            ]
+            return ont_id
+        else:
+            match_ont = [
+                re.search("'[0-9]{1,5}'", alrm) for alrm in alrm_tbl.split("\n")
+            ]
+            ont_id = [
+                m.group().lstrip("'").rstrip("'") for m in match_ont if m is not None
+            ]
+            return ont_id
 
     return inner
 
@@ -27,18 +52,19 @@ def proc_alarms(func):
 def alarm_table(e9=argv[2]):
     cnct = calix_e9()
     tbl = input(f"{c_BLUE}Alarm name: {c_WHITE}")
-    dying = cnct.send_command_timing("show alarm active | include dying")
-    missing = cnct.send_command_timing("show alarm active | include missing")
-
-    # NOTE: Send this through another decorator first to get the ont_ids
-
-    # lop = cnct.send_command_timing("show alarm active | include loss")
     if tbl == "dying":
+        dying = cnct.send_command_timing("show alarm active | include dying")
+        cnct.disconnect()
         return dying
     elif tbl == "missing":
+        missing = cnct.send_command_timing("show alarm active | include missing")
+        cnct.disconnect()
         return missing
+    elif tbl == "lop":
+        lop = cnct.send_command_timing("show alarm active | include loss")
+        return lop
     else:
-        print('Please use "dying" or "missing"')
+        print('Valid completions: "dying", "missing", "lop"')
         alarm_table(e9=argv[2])
 
 
