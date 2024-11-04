bd53593dbc75d7e3ea4eb436c0a047da98641ced
Derrick Ishman <derrick.ishman@gmail.com>
2024-11-04 16:42:58 -0600

    quick fix to get active customers, fix properly later

--- get_act_cx.py
+++ get_act_cx.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from requests import get
 from sys import argv
-from calix import ssp, netcon
 
+from calix.connection import calix_e9
+from calix.ssp import ssp
+from requests import get
 
 # NOTE: Call script with IP address, hostname
 
@@ -21,6 +22,8 @@ def get_act(func):
                 r = get_cx.json()
                 if r.get("locations") is None:
                     continue
+                phone = r.get("locations")[0].get("contacts")[0].get("phone")
+                em = r.get("locations")[0].get("contacts")[0].get("email")
                 loc, city, state, z = r.get("locations")[0].get("address")[0].values()
                 print(r.get("customId"))
                 print(r.get("name"))
@@ -39,30 +42,42 @@ def get_act(func):
                         verify=False,
                     )
                     r = get_cx.json()
-                    if r.get("locations") is None:
-                        continue
-                    loc, city, state, z = (
-                        r.get("locations")[0].get("address")[0].values()
-                    )
-                    print(r.get("customId"))
-                    print(r.get("name"))
-                    print(loc)
-                    print(f"{city} {state} {z}\n")
+                    try:
+                        name = r.get("name")
+                        acct = r.get("customId")
+                        phone = r.get("locations")[0].get("contacts")[0].get("phone")
+                        em = r.get("locations")[0].get("contacts")[0].get("email")
+                        loc = (
+                            r.get("locations")[0].get("address")[0].get("streetLine1")
+                            + ", "
+                            + r.get("locations")[0].get("address")[0].get("city")
+                        )
+                    except Exception:
+                        if name or acct is None:
+                            continue
+                    else:
+                        if phone is None or phone == "":
+                            phone = "No phone"
+                        if em is None or em == "":
+                            em = "No email"
+                        if loc is None or loc == "":
+                            loc = "No location"
+                    print(f"{acct}\n{name}\n{phone}\n{em}\n{loc}\n")
         return onts
 
     return wrapper
 
 
-@get_act
+# @get_act
 def get_miss(main_f):
     def missing():
         func = main_f()
 
 
-# @get_act
+@get_act
 def main():  # NOTE: Returns list[lists], or a single list
     shelf, slot, port = ssp()
-    cnct = netcon()
+    cnct = calix_e9()
     ports = range(1, 17)
     slots = range(1, 3)
     step_2 = slice(1, None, 2)
