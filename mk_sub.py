3818a8fa72954fa26b015820b3dd6e360a0d3293
Derrick Ishman <derrick.ishman@gmail.com>
2024-11-22 13:41:33 -0600

    Merged from new-tools branch

---
+++ mk_sub.py
@@ -0,0 +1,50 @@
+from requests import post
+
+name = input("Name: ")
+acct = input("Acct: ")
+address = input("Address: ")
+city = input("City: ")
+state = input("State: ")
+zip = input("Zip: ")
+fname = input("First: ")
+lname = input("Last: ")
+phone = input("Phone: ")
+email = input("Email: ")
+
+payload = {
+    "name": name,
+    "customId": acct,
+    "type": "Residential",
+    "orgId": "Calix",
+    "locations": [
+        {
+            "primary": True,
+            "address": [
+                {
+                    "streetLine1": address,
+                    "city": city,
+                    "state": state,
+                    "zip": zip,
+                }
+            ],
+            "contacts": [
+                {
+                    "firstName": fname,
+                    "lastName": lname,
+                    "phone": phone,
+                    "email": email,
+                    "primary": True,
+                }
+            ],
+        }
+    ],
+}
+
+sub = post(
+    "https://10.20.7.10:18443/rest/v1/ems/subscriber",
+    auth=("admin", "Thesearethetimes!"),
+    verify=False,
+    json=payload,
+)
+if sub.status_code == 201:
+    print(f"\nSubscriber created successfully!")
