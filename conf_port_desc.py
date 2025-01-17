549614b10f329176b47b2403e66a364089cc6497
Derrick Ishman <derrick.ishman@gmail.com>
2025-01-17 15:20:27 -0600

    Configured chisney ports

--- conf_port_desc.py
+++ conf_port_desc.py
@@ -2,21 +2,29 @@ from time import sleep
 
 from netmiko import ConnectHandler
 
+f1 = "n1"
+f2 = "w2"
+f3 = "s3"
+f4 = "xw2"
+ports = range(1, 24, 2)
+fibers = (f for f in range(117, 142) if f % 12 != 0)
+# for p in ports:
+#    print(f"5/1/xp{p}_{f4},{next(fibers)}-{next(fibers)}")
+
 device = {
     "device_type": "cisco_ios",
-    "host": '10.20.5.51',
+    "host": "10.20.5.51",
     "username": "sysadmin",
     "password": "Thesearethetimes!",
     "fast_cli": False,
 }
 cnct = ConnectHandler(**device)
-cnct.send_command_timing("configure")
-output = cnct.send_command_timing("copy running-config startup-config")
 
-
-fn1 = 'n1'
-fw2 = 'w2'
-fs3 = 's3'
-fxw2 = 'xw2'
-a = range(1, 12)
-port = 
+for p in ports:
+    cnct.send_command_timing("configure")
+    cnct.send_command_timing(f"interface pon 5/2/xp{p}")
+    cnct.send_command_timing(f"description {f4},{next(fibers)}-{next(fibers)}")
+    output = cnct.send_command_timing(f"top show full-configuration int pon 5/2/xp{p}")
+    print(output)
+    sleep(1)
+    print("")
---
+++ get_act_cx.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+from calix.connection import calix_e9
+from calix.ssp import ssp
+from requests import get
+
+# NOTE: Call script with IP address, hostname
+
+
+def get_act(func):
+    def wrapper(*args):
+        onts = func(*args)
+        if isinstance(onts, list):
+            for id in onts:
+                get_cx = get(
+                    f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{argv[2]}/port/{id}%2Fx1",
+                    auth=("admin", "Thesearethetimes!"),
+                    verify=False,
+                )
+                r = get_cx.json()
+                if r.get("locations") is None:
+                    continue
+                phone = r.get("locations")[0].get("contacts")[0].get("phone")
+                em = r.get("locations")[0].get("contacts")[0].get("email")
+                loc, city, state, z = r.get("locations")[0].get("address")[0].values()
+                print(r.get("customId"))
+                print(r.get("name"))
+                print(loc)
+                print(f"{city} {state} {z}\n")
+
+        else:
+            for lst in onts:
+                if lst is None:
+                    continue
+                for id in lst:
+                    get_cx = get(
+                        f"https://10.20.7.10:18443/rest/v1/ems/subscriber/device/{argv[2]}/port/{id}%2Fx1",
+                        auth=("admin", "Thesearethetimes!"),
+                        verify=False,
+                    )
+                    r = get_cx.json()
+                    try:
+                        name = r.get("name")
+                        acct = r.get("customId")
+                        phone = r.get("locations")[0].get("contacts")[0].get("phone")
+                        em = r.get("locations")[0].get("contacts")[0].get("email")
+                        loc = (
+                            r.get("locations")[0].get("address")[0].get("streetLine1")
+                            + ", "
+                            + r.get("locations")[0].get("address")[0].get("city")
+                        )
+                    except Exception:
+                        if name or acct is None:
+                            continue
+                    else:
+                        if phone is None or phone == "":
+                            phone = "No phone"
+                        if em is None or em == "":
+                            em = "No email"
+                        if loc is None or loc == "":
+                            loc = "No location"
+                    print(f"{acct}\n{name}\n{phone}\n{em}\n{loc}\n")
+        return onts
+
+    return wrapper
+
+
+@get_act
+def main():  # NOTE: Returns list[lists], or a single list
+    shelf, slot, port = ssp()
+    cnct = calix_e9()
+    ports = range(1, 17)
+    slots = range(1, 3)
+    step_2 = slice(1, None, 2)
+    if not slot == "" and port == "":
+        onts = (
+            cnct.send_command_timing(
+                f"show interface pon {shelf}/{slot}/xp{p} ranged-onts \
+            statistics | inc ont-id"
+            ).split()[step_2]
+            for p in ports
+        )
+    elif slot == "":
+        onts = (
+            cnct.send_command_timing(
+                f"show interface pon {shelf}/{sl}/xp{p} ranged-onts \
+            statistics | inc ont-id"
+            ).split()[step_2]
+            for sl in slots
+            for p in ports
+        )
+    else:
+        onts = cnct.send_command_timing(
+            f"show interface pon {shelf}/{slot}/xp{port} ranged-onts \
+            statistics | inc ont-id"
+        ).split()[step_2]
+    return onts
+
+
+if __name__ == "__main__":
+    main()
