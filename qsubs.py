110286b748dd16561e62cff70d4d10d902158674
Derrick Ishman <derrick.ishman@gmail.com>
2025-01-17 09:45:23 -0600

    changes to affected_decorator requires calling with IP address in qsubs

--- qsubs.py
+++ qsubs.py
@@ -3,19 +3,18 @@
 from sys import argv
 
 from calix.affected_decorator import affected_decorator
-
 from crayon import c_BLUE, c_CYAN, c_WHITE
 
 onts = list(input(f"{c_BLUE}ONT_IDS{c_WHITE}: ").split())
 
 
 @affected_decorator
-def main(e9=argv[1]):
+def main(e9=argv[2]):
     return onts
 
 
 if __name__ == "__main__":
-    subs = main(e9=argv[1])
+    subs = main(e9=argv[2])
     for sub in subs:
         print(sub)
     q = input(f"{c_CYAN}Press any key to exit...")
