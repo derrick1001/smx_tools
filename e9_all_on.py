9a2b7c5843704b5a8f2b5f2ef73245ec6db03dc0
Derrick Ishman <twohackerauth@proton.me>
2025-02-20 15:56:50 -0600

    feat: added functionality for 3201 cards

--- e9_all_on.py
+++ e9_all_on.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-from netmiko import ConnectHandler
 from sys import argv
+
+from netmiko import ConnectHandler
 
 #   TODO:
 #   Add functionality for 3201 cards
@@ -18,16 +19,24 @@ def turn_on_ports():  # NOTE: Call with starting shelf and ending shelf
     }
     shelves = range(int(argv[1]), int(argv[2]) + 1)
     slot = range(1, 3)
-    port = range(1, 17)
     with ConnectHandler(**device) as cnct:
         cnct.send_command_timing("configure")
         for shelf in shelves:
             for sl in slot:
+                card = cnct.send_command_timing(
+                    f"do show card {shelf}/{sl} |\
+                notab | include provision"
+                )
+                print(f"{shelf}/{sl}")
+                if "NG1601" in card.split()[1]:
+                    print("Card is 1601")
+                    port = range(15, 17)
+                else:
+                    print("Card is 3201")
+                    port = range(1, 33)
                 for p in port:
-                    cnct.send_command_timing(
-                        f"interface pon {shelf}/{sl}/xp{p}")
-                    cnct.send_command_timing(
-                        f"interface pon {shelf}/{sl}/xp{p}")
+                    cnct.send_command_timing(f"interface pon {shelf}/{sl}/xp{p}")
+                    cnct.send_command_timing(f"interface pon {shelf}/{sl}/xp{p}")
                     cnct.send_command_timing("no shutdown")
                     cnct.send_command_timing("top")
                     print(f"{shelf}/{sl}/xp{p} is on")
