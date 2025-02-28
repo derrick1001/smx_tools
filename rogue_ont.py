bca17205c956c032bf7d2f17f10b1b127a0c0d2b
Derrick Ishman <derrick.ishman@gmail.com>
2025-02-28 12:21:02 -0600

    fix: multiple alarms can exist, change to write to file and email

--- rogue_ont.py
+++ rogue_ont.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from subprocess import run
 from sys import argv
 
 from calix.connection import calix_e9
@@ -10,14 +11,18 @@ from calix.proc_alrms import proc_alarms
 @proc_alarms
 def rogue(e9=argv[2]):
     cnct = calix_e9()
-    red_temp = cnct.send_command_timing(
+    rogue_alrm = cnct.send_command_timing(
         "show alarm active | include ont-rogue-detected"
     )
     cnct.disconnect()
-    return rogue
+    return rogue_alrm
 
 
 if __name__ == "__main__":
-    sub = rogue(e9=argv[2])
-    subj = f"Rogue ONT detected on {argv[2]}"
-    email(subj, next(sub))
+    subs = rogue(e9=argv[2])
+    with open("rogues.txt", "a") as f:
+        for sub in subs:
+            f.write(f"{sub}\n")
+    with open("rogues.txt", "r+") as f:
+        email(f"Rogue ONTs detected on {argv[2]}", f.read())
+    run("rm rogues.txt", shell=True)
