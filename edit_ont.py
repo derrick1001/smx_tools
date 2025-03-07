36bf8dc70892553477a1e828c8d4e26bb1d62a9b
Derrick Ishman <derrick.ishman@gmail.com>
2025-03-07 11:06:47 -0600

    feat: added automatic discovery of new ONT

--- edit_ont.py
+++ edit_ont.py
@@ -1,12 +1,27 @@
+from calix.connection import calix_e9
 from requests import put
 
-id = ["2001", "2002", "2003", "2004", "2005"]
+ont = ["2001", "2002", "2003", "2004", "2005"]
+cnct = calix_e9()
+show_discovered = cnct.send_command_timing(
+    "show interface pon 2/1/xp1 discovered-onts | notab | inc CXNK"
+).split()[2::3]
+cnct.disconnect()
 
-for i in id:
-    sn = input("SN: ")
+cxnk = []
+for i in show_discovered:
+    if len(i) == 7:
+        i = f"0{i}"
+    elif len(i) == 6:
+        i = f"00{i}"
+    cxnk.append(i)
+print(cxnk)
+
+
+for id, sn in zip(ont, cxnk):
     payload = {
         "serial-number": sn,
-        "ont-id": i,
+        "ont-id": id,
         "ont-profile-id": "GP1100X",
     }
     service = put(
