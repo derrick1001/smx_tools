fba5eb485cecbb14bb6201cf1eaa122c73cd08f2
Derrick Ishman <derrick.ishman@gmail.com>
2025-07-14 14:32:20 -0500

    fix: added error handling for authentication and network failures

--- e9_all_on.py
+++ e9_all_on.py
@@ -1,24 +1,37 @@
 #!/usr/local/bin/python3
 
 from sys import argv
-
-from netmiko import ConnectHandler
-
-#   TODO:
-#   Test to see if threading will speed this up
+from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
 
 
-def turn_on_ports():  # NOTE: Call with starting shelf and ending shelf
+def default_or_not():
     device = {
         "device_type": "cisco_ios",
-        "host": "192.168.1.1",
+        "host": "10.20.0.51",
         "username": "sysadmin",
         "password": "sysadmin",
-        "fast_cli": False,
+        "fast_cli": True,
     }
-    shelves = range(int(argv[1]), int(argv[2]) + 1)
+    device_configured = {
+        "device_type": "cisco_ios",
+        "host": "10.20.0.51",
+        "username": "sysadmin",
+        "password": "Thesearethetimes!",
+        "fast_cli": True,
+    }
+    try:
+        cnct = ConnectHandler(**device)
+        return cnct
+    except NetmikoTimeoutException:
+        print("Network not reachable, check network settings")
+    except NetmikoAuthenticationException:
+        return ConnectHandler(**device_configured)
+
+
+def turn_on_ports(cnct, start: int, stop: int):
+    shelves = range(start, stop + 1)
     slot = range(1, 3)
-    with ConnectHandler(**device) as cnct:
+    with cnct:
         cnct.send_command_timing("configure")
         for shelf in shelves:
             for sl in slot:
@@ -26,25 +39,27 @@ def turn_on_ports():  # NOTE: Call with starting shelf and ending shelf
                     f"do show card {shelf}/{sl} |\
                 notab | include provision"
                 )
-                print(f"{shelf}/{sl}")
                 if "NG1601" in card.split()[1]:
-                    print("Card is 1601")
+                    print(f"{shelf}/{sl} card is 1601")
                     port = range(1, 17)
                 elif "XG3201" in card.split()[1]:
-                    print("Card is 3201")
+                    print(f"{shelf}/{sl} card is 3201")
                     port = range(1, 33)
                 else:
                     print("Card not in service, use show card to verify")
-                    quit()
                 for p in port:
+                    cmd_list = [f"interface pon {shelf}/{sl}/xp{p}", "no shutdown", "top"]
+                    cmd_as_string = '\n'.join(cmd_list)
                     cnct.send_command_timing(
-                        f"interface pon {shelf}/{sl}/xp{p}")
-                    cnct.send_command_timing(
-                        f"interface pon {shelf}/{sl}/xp{p}")
-                    cnct.send_command_timing("no shutdown")
-                    cnct.send_command_timing("top")
+                        cmd_as_string)
                     print(f"{shelf}/{sl}/xp{p} is on")
 
 
 if __name__ == "__main__":
-    turn_on_ports()
+    try:
+        int(argv[1]) and int(argv[2]) in range(2, 6)
+        cnct = default_or_not()
+        if cnct is not None:
+            turn_on_ports(cnct, int(argv[1]), int(argv[2]))
+    except ValueError:
+        print("Wrong format, use command python3 e9_all_on.py <2-5> <2-5>")
