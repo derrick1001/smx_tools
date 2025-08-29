510bc7abe068d2f66f959e170f55a72c349a95f6
Derrick Ishman <derrick.ishman@gmail.com>
2025-08-29 16:04:13 -0500

    changes

--- get_act_ports.py
+++ get_act_ports.py
@@ -3,23 +3,21 @@ from calix.ssp import ssp
 shelf, slot, port = ssp()
 
 
-def get_ranges(shelf: str, slot: str, port: str) -> tuple(range, range):
+def get_ranges(slot: str, port: str) -> tuple[range, range]:
     if slot == "":
         slot_range = range(1, 3)
+    else:
+        slot_range = slot
     if "-" in port:
-        port_range = range(port.split("-")[0], port.split("-")[1] + 1)
-    elif port == "":
+        port_range = range(int(port.split("-")[0]), int(port.split("-")[1]) + 1)
+    else:
         port_range = range(1, 17)
     return slot_range, port_range
 
 
-def main():
-    pass
-
-
 if __name__ == "__main__":
     # cnct = calix_e9()
-    slots, ports = get_ranges()
-    for slot in slots:
-        for port in ports:
-            print(f"{shelf}{slot}/xp{port}")
+    slots, ports = get_ranges(slot, port)
+    ranges = [f"{shelf}/{slot}/xp{port}" for slot in slots for port in ports]
+    for port in ranges:
+        print(port)
