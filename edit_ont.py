dd051769a5372053a78c94aa05d0a796c46dbd48
Derrick Ishman <twohackerauth@proton.me>
2026-01-09 10:08:56 -0600

    fixed issue with function rc500 in edit_ont.py

--- edit_ont.py
+++ edit_ont.py
@@ -32,8 +32,8 @@ def rcode_500(id: str, sn: str, mod: str):
     print(
         f"\n{c_RED}Serial number {c_MAGENTA}CXNK{sn} {c_RED}already in use, force deleting and reassigning")
     sleep(2)
-    get_id = get(f"https://10.20.7.10:18443/rest/v1/config/device/CVEC-E9-1/ont?serial-number=CXNK{sn}", 
-                 auth=("admin", "Thesearethetimes!"), 
+    get_id = get(f"https://10.20.7.10:18443/rest/v1/config/device/CVEC-E9-1/ont?serial-number=CXNK{sn}",
+                 auth=("admin", "Thesearethetimes!"),
                  verify=False)
     nid = get_id.json().get("ont-id")
     print(f"{c_CYAN}Deleting old ONT...")
@@ -97,7 +97,8 @@ if __name__ == "__main__":
                     "subscriber-id": id,
                 }
                 service = put(
-                    f"https://10.20.7.10:18443/rest/v1/config/device/CVEC-E9-1/ont?action=update&ont-id={id}&serial-number=CXNK{sn}",
+                    f"https://10.20.7.10:18443/rest/v1/config/device/CVEC-E9-1/ont?action=update&ont-id={
+                        id}&serial-number=CXNK{sn}",
                     auth=("admin", "Thesearethetimes!"),
                     verify=False,
                     json=payload,
