76afe2b42629276ad6811c6994f2f6988f552e8a
Derrick Ishman <twohackerauth@proton.me>
2026-03-09 16:07:20 -0500

    exclude ONT being used for wifi in testing lab

--- edit_ont.py
+++ edit_ont.py
@@ -40,7 +40,7 @@ def get_discovered():
     # NOTE:
     # If discovering both ports is necessary, do it here and join the lists together with sh_ont.extend()
     #
-    sh_ont = cvec.connection.send_command_timing("show interface pon 2/1/xp2 discovered-onts | notab | inc discovered").split()[3::3]
+    sh_ont = cvec.connection.send_command_timing("show interface pon 2/1/xp2 discovered-onts | notab | inc discovered-ont | exclude 9A3F1A").split()[3::3]
     cxnk = [f"0{i}" if len(i) == 7 else f"00{i}" for i in sh_ont]
     return cxnk
 
