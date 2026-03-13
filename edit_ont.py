b72e4a01263b3234b6357939bd5cbecb570a2168
Derrick Ishman <twohackerauth@proton.me>
2026-03-13 16:42:15 -0500

    refactored rcode_500 function, it now just replaces the SN with a randomly generated SN. This should prevent already in use errors when moving ONTs around on the test bench

--- edit_ont.py
+++ edit_ont.py
@@ -1,6 +1,7 @@
 from warnings import filterwarnings
 from sys import path
 from time import sleep
+from random import choice
 import logging
 
 import auth
@@ -106,48 +107,14 @@ def make_payload(id: str, sn: str) -> tuple[dict, dict]:
 
 
 def rcode_500(id: str, sn: str):
-    new_ont_payload, services_payload = make_payload(id, sn)
-    print(f"\n{c_RED}Serial number {c_MAGENTA}CXNK{sn} {c_RED}already in use, deleting and reassigning...")
-    sleep(2)
-    for hostname in e9.keys():
-        get_id = get(f"https://10.20.7.10:18443/rest/v1/config/device/{hostname}/ont?serial-number=CXNK{sn}",
-                     auth=(auth.username, auth.password),
-                     verify=False)
-        if get_id.status_code == 200:
-            print(f"{c_MAGENTA}{sn}{c_WHITE} found on {c_GREEN}{hostname}")
-            if hostname == 'CVEC-E9-1':
-                print(f"{c_CYAN}Deleting old ONT...")
-                rmont(id, cvec.name)
-                sleep(2)
-                print(f"{c_CYAN}Making new ONT...")
-                mk_ont(cvec.name, **new_ont_payload)
-                sleep(2)
-                print(f"{c_CYAN}Applying services...")
-                mk_eth_serv(**services_payload)
-                break
-            nid = get_id.json()[0].get("ont-id")
-            sleep(2)
-            print(f"{c_CYAN}Deleting old ONT...")
-            rmont(nid, hostname)
-            sleep(1)
-            rmont(id, cvec.name)
-            sleep(1)
-            print(f"{c_CYAN}Making new ONT...")
-            mk_ont(cvec.name, **new_ont_payload)
-            sleep(2)
-            print(f"{c_CYAN}Applying services...")
-            mk_eth_serv(**services_payload)
-            sleep(2)
-            validate = get(f"https://10.20.7.10:18443/rest/v1/config/device/{cvec.name}/ont?serial-number=CXNK{sn}",
-                           auth=(auth.username, auth.password),
-                           verify=False)
-            if validate.status_code == 200:
-                print(f"\nONT {c_MAGENTA}{sn} {c_WHITE}successfully updated with account {c_CYAN}{id}")
-                return 0
-        elif get_id.status_code == 404:
-            print(f"{c_MAGENTA}{sn} {c_WHITE}not found on {c_CYAN}{hostname}, searching...")
-            sleep(1)
-            continue
+    CHARACTERS = "ABCDEF"
+    LENGTH = 8
+
+    print(f"\n{c_RED}Serial number {c_MAGENTA}CXNK{sn} {c_RED}already assigned, swapping...")
+    get_id = ont(cvec.name, id)
+    old_id = get_id.json().get("ont-id")
+    random_cxnk = "".join(choice(CHARACTERS) for _ in range(LENGTH + 1))
+    kansas_city_shuffle(old_id, random_cxnk)
 
 
 def kansas_city_shuffle(id, sn) -> int:
@@ -180,7 +147,6 @@ if __name__ == "__main__":
                     print(levels)
                 elif service.status_code == 500 or service.status_code == 403:
                     rcode_500(id, sn)
-                    sleep(2)
                     levels = get_light(id)
                     print(levels)
                 elif service.status_code == 404:
