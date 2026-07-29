1088721b247104576e85ac8c2980406d120fb5d9
Derrick Ishman <twohackerauth@proton.me>
2026-07-29 15:09:33 -0500

    trying server module import again

--- edit_ont.py
+++ edit_ont.py
@@ -4,6 +4,7 @@ from time import sleep
 import logging
 
 from calix.auth import username, password
+from calix.server import SMX
 from calix.e9 import CalixE9
 from calix.ont_detail import ont
 from calix.post_eth_serv import mk_eth_serv
@@ -125,7 +126,7 @@ def rcode_500(id: str, sn: str):
 
 
 def kansas_city_shuffle(id: str, sn: str):
-    service = put(f"https://10.20.17.10:18443/rest/v1/config/device/{cvec.name}/ont?action=replace&ont-id={id}&serial-number=CXNK{sn}",
+    service = put(f"https://{SMX}.10:18443/rest/v1/config/device/{cvec.name}/ont?action=replace&ont-id={id}&serial-number=CXNK{sn}",
                   auth=(username, password),
                   verify=False,
                   )
